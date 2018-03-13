c5a79432f087a845ca0022855a792a562d335d57
Eran Schweitzer <eranschweitzer@gmail.com>
2018-03-12 18:45:25 -0700

    Generation Qlimits
    Some generators have very large Qlimits (essentiall the equivalent of
    inifinit). I suspect that when these are multiplying the Pi permutation
    matrix it is causing all sorts of numerical issues, which result in
    infeasibility or unboundedness.
    
    This is based on an observation that the zones that appeared to
    constantly be returning problems were the ones that had such infinit
    limits while the others were not. Far from conclusive, but semi
    convincing.
    
    In formulation_ea a variable is created called Qgmax, which is a copy of
    the input params['S']['Qgmax']. Then every entry that is larger than
    twice the sum of all the reactive load in the zone
    (2*params['S']['Qd'].sum()) is replaced by 2*params['S']['Qd'].sum().
    
    Qgmax is then used in creating the constraints, however it is NOT used
    in exporting the variables, that way the final solution should still
    have the "correct" limits

--- formulation_ea.py
+++ formulation_ea.py
@@ -90,6 +90,12 @@ class ZoneMILP(object):
         Y = hlp.Yparts(params['z']['r'], params['z']['x'], b=params['z']['b'], tau=params['z']['tap'], phi=params['z']['shift'])
         sil = hlp.calc_sil(**params['z'])
 
+        ## replace obsurdely large Qlimits with simply large limits
+        ## hopefully this avoids some of the feasibility/unboundedness issues
+        Qgmax = params['S']['Qgmax'].copy()
+        Qgmax_tmp = 2*params['S']['Qd'].sum()
+        Qgmax[Qgmax > Qgmax_tmp] = Qgmax_tmp
+
         
         ### save inputs
         self.N = N; self.L = L
@@ -303,8 +309,8 @@ class ZoneMILP(object):
             ### gen
             self.m.addConstrs( self.Pg[i] <=  sum( self.Pi[i,j]*params['S']['Pgmax'][j] for j in range(N) )/100 for i in range(N))
             self.m.addConstrs( self.Pg[i] >=  sum( self.Pi[i,j]*params['S']['Pgmin'][j] for j in range(N) )/100 for i in range(N))
-            self.m.addConstrs( self.Qg[i] <=  sum( self.Pi[i,j]*params['S']['Qgmax'][j] for j in range(N) )/100 for i in range(N))
-            self.m.addConstrs( self.Qg[i] >= -sum( self.Pi[i,j]*params['S']['Qgmax'][j] for j in range(N) )/100 for i in range(N))
+            self.m.addConstrs( self.Qg[i] <=  sum( self.Pi[i,j]*Qgmax[j] for j in range(N) )/100 for i in range(N))
+            self.m.addConstrs( self.Qg[i] >= -sum( self.Pi[i,j]*Qgmax[j] for j in range(N) )/100 for i in range(N))
         else:
             ### load
             self.m.addConstrs( self.Pd[i] == params['S']['Pd'][rnmap[i]]/100 for i in range(N))
@@ -312,8 +318,8 @@ class ZoneMILP(object):
             ### gen
             self.m.addConstrs( self.Pg[i] <=  params['S']['Pgmax'][rnmap[i]] /100 for i in range(N))
             self.m.addConstrs( self.Pg[i] >=  params['S']['Pgmin'][rnmap[i]] /100 for i in range(N))
-            self.m.addConstrs( self.Qg[i] <=  params['S']['Qgmax'][rnmap[i]] /100 for i in range(N))
-            self.m.addConstrs( self.Qg[i] >= -params['S']['Qgmax'][rnmap[i]] /100 for i in range(N))
+            self.m.addConstrs( self.Qg[i] <=  Qgmax[rnmap[i]] /100 for i in range(N))
+            self.m.addConstrs( self.Qg[i] >= -Qgmax[rnmap[i]] /100 for i in range(N))
 
        
         ### nodal balance
